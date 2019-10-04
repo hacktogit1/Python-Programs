761c1d7c507ea75f1950dcd66463e869f31945c5
hacktogit1 <56072510+hacktogit1@users.noreply.github.com>
2019-10-04 18:36:18 +0530

    Create addition.py
    Create Addition file

---
+++ addition.py
@@ -0,0 +1,7 @@
+# In Python3, addition can be done very easily with numbers and strings
+7 + 2
+# By writing 7+2 it directly gives the addition output
+
+# Strings :-
+'Hello' + 'World'
+# Above code will merge 2 strings and gives output HelloWorld
